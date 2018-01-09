bd7ff4d2bd3e84b96d9f0f0cebb81a7e04649d43
github username <github email address>
2018-01-09 10:11:26 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ToggleButton from "./components/ToggleButton";
 
 ReactDOM.render(
-  <div>Components Need To Be Mounted</div>,
-  document.getElementById('root')
-)
+  <div>
+    <ToggleButton />
+  </div>,
+  document.getElementById("root")
+);
